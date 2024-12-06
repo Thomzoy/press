5b759fe3df540ccfa9598b1b68de216fe8b805ff
Thomas Petit-Jean <30775613+Thomzoy@users.noreply.github.com>
2024-12-06 00:32:18 +0000

    fix

--- nav/login.py
+++ nav/login.py
@@ -11,11 +11,11 @@ def setup_headless_browser():
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Run in background
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--enable-logging")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--enable-logging")
     chrome_options.add_argument("--v=1")
-    chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
-    chrome_options.add_experimental_option("perfLoggingPrefs", {"enableNetwork": True})
+    # chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
+    # chrome_options.add_experimental_option("perfLoggingPrefs", {"enableNetwork": True})
 
     driver = webdriver.Chrome(options=chrome_options)
 
